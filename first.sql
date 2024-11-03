7142b0350d1766cab8d2ddcc421af4f21d04132a
ScarKing097 <dhavalv956@gmail.com>
2024-11-03 10:53:10 +0100

    Create first.sql

---
+++ first.sql
@@ -0,0 +1,8 @@
+#select * from users where ID=1191; 
+
+#SELECT MIN(ID) FROM USERS;
+#SELECT MAX(ID) FROM USERS;
+#SELECT * FROM users WHERE name in ('Patrick', 'David', 'Jason');
+
+#UPDATE users SET name='Scarking' WHERE ID=1191;
+DELETE FROM users WHERE ID=7;
